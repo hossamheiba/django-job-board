31b51864e9ea24593e793b614825ad7e7e0c1c11
hossamheiba <105421456+hossamheiba@users.noreply.github.com>
2022-05-15 16:55:23 +0300

    add url for apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My_Apps
+    'blog.apps.BlogConfig',
+    'contact.apps.ContactConfig',
+    'accounts.apps.AccountsConfig',
+    'job.apps.JobConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',include("home.url")),
+    path('blog/',include("blog.url")),
+    path('accounts/',include("accounts.url")),
+    path('job/',include("job.url")),
+    path('contact/',include("contact.url")),
 ]
